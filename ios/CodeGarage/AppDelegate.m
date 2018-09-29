3635ac19ad6f024fb49365ffae427724e303f8d3
Ema Lorenzo <aemalorenzo@gmail.com>
2018-09-29 17:30:53 -0300

    Firebase native integration

--- ios/CodeGarage/AppDelegate.m
+++ ios/CodeGarage/AppDelegate.m
@@ -10,6 +10,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+@import Firebase;
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -29,6 +30,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [FIRApp configure];
   return YES;
 }
 
