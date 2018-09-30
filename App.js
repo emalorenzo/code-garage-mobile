db263c9f28b66a1197e4bb4b1bcc4bb6a21e6add
Ema Lorenzo <aemalorenzo@gmail.com>
2018-09-30 03:09:21 -0300

    Google Signin Working

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import firebase from 'react-native-firebase';
-import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin'
+import { GoogleSignin, GoogleSigninButton, statusCodes } from 'react-native-google-signin'
 
 class App extends React.Component {
 
@@ -9,6 +9,8 @@ class App extends React.Component {
     super();
     this.state = {
       isAuthenticated: false,
+      isSigninInProgress: false,
+      user: undefined
     };
   }
 
@@ -17,26 +19,76 @@ class App extends React.Component {
       .then(() => {
         this.setState({
           isAuthenticated: true,
-        });
-      });
+        })
+      })
+  }
+
+  signIn = async () => {
+    try {
+      this.setState({ isSigninInProgress: true })
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      this.setState({ user: userInfo.user, isSigninInProgress: false })
+
+      // create a new firebase credential with the token
+      const credential = firebase.auth.GoogleAuthProvider.credential(userInfo.idToken, userInfo.accessToken)
+      // login with credential
+      const currentUser = await firebase.auth().signInWithCredential(credential)
+
+      console.info(JSON.stringify(currentUser.user.toJSON()));
+
+    } catch (error) {
+      this.setState({ isSigninInProgress: false })
+      console.log('error', error)
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (f.e. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  }
+
+  signOut = async () => {
+    try {
+      await GoogleSignin.revokeAccess()
+      await GoogleSignin.signOut()
+      this.setState({ user: null }) // Remember to remove the user from your app's state as well
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  _signIn () {
+    GoogleSignin.configure()
+    this.signIn()
   }
 
   render() {
     // If the user has not authenticated
     if (!this.state.isAuthenticated) {
-      return null;
+      return null
     }
 
     return (
-      <View>
-        <Text>Welcome to my awesome app!</Text>
+      <View style={{ padding: 50 }}>
+        <Text>{`Hola${this.state.user ? ' ' + this.state.user.givenName + '!' : '!'}`}</Text>
         <GoogleSigninButton
-          style={{ width: 48, height: 48 }}
-          size={GoogleSigninButton.Size.Icon}
-          color={GoogleSigninButton.Color.Dark}
-          onPress={this._signIn}
+          style={{ width: '100%', height: 60 }}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Light}
+          onPress={() => this._signIn()}
           disabled={this.state.isSigninInProgress}
         />
+        <TouchableOpacity
+          onPress={() => this.signOut()}
+          style={{ margin: 20, padding: 20 }}
+        >
+          <Text>Logout</Text>
+        </TouchableOpacity>
       </View>
     );
   }
